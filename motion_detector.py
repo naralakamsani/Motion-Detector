dbb8269044fbae35babe18e5d8f555236e89bb58
Narasimha lakamsani <56650712+naralakamsani@users.noreply.github.com>
2020-06-08 12:39:01 -0400

    Add files via upload

---
+++ motion_detector.py
@@ -0,0 +1,66 @@
+import cv2
+from datetime import datetime
+
+import pandas
+
+first_frame = None
+
+video = cv2.VideoCapture(0)
+
+status_list = [None, None]
+times = []
+
+while True:
+    check, frame = video.read()
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame
+        continue
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame)
+
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=1)
+
+    (cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    status = 0
+    for countour in cnts:
+        if cv2.contourArea(countour) < 1000:
+            continue
+        status = 1
+
+        (x, y, w, h) = cv2.boundingRect(countour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+
+    status_list.append(status)
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.now())
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        times.append(datetime.now())
+
+    cv2.imshow("gray", gray_frame)
+    cv2.imshow("delta", delta_frame)
+    cv2.imshow("thresh", thresh_frame)
+    cv2.imshow("frame", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        if status_list == 1:
+            times.append(datetime.now)
+        break
+
+print(times)
+
+df = pandas.DataFrame(columns=["Start", "End"])
+for i in range(0, len(times), 2):
+    df = df.append({"Start": times[i], "End": times[i + 1]}, ignore_index=True)
+
+    df.to_csv("Time of motion.csv")
+
+video.release()
+cv2.destroyAllWindows()
---
+++ plot_motion_data.py
@@ -0,0 +1,20 @@
+from bokeh.models import HoverTool, ColumnDataSource
+from bokeh.plotting import figure, show, output_file
+from motion_detector import df
+
+df["Start_string"]=df["Start"].dt.strftime("%Y-%m-%d-%H:%M:%S")
+df["End_string"]=df["End"].dt.strftime("%Y-%m-%d-%H:%M:%S")
+
+cds = ColumnDataSource(df)
+
+f = figure(x_axis_type='datetime', height=150, width=800, title="Motion Graph")
+f.yaxis.minor_tick_line_color = None
+f.ygrid[0].ticker.desired_num_ticks = 1
+
+hover = HoverTool(tooltips=[("Start","@Start_string"),("End","@End_string")])
+f.add_tools(hover)
+
+p = f.quad(left="Start", right="End", bottom=0, top=1, color="green", source=cds)
+
+output_file("motion_graph.html")
+show(f)
