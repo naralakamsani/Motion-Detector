ca3d54cd21507a46c2cf77b8e27ec58ad051e76b
naralakamsani <nc.lakamsani@gmail.com>
2020-09-22 18:56:42 -0400

    updated comments

--- motion_detector.py
+++ motion_detector.py
@@ -29,18 +29,18 @@ while True:
 
     #convert delta frame to thresh fram
     thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
-    thresh_frame = cv2.dilate(thresh_fram, None, iterations=1)
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=1)
 
     #Find the contours or objects not in the first frame
     (cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    status = 0 //#set status to 0 to start
+    status = 0 #set status to 0 to start
     
     #find contours or objects not in first frame that are relatively large
     for countour in cnts:
         if cv2.contourArea(countour) < 1000:
             continue
-        status = 1 //#set status to 1 if relatively large contours is found
+        status = 1 #set status to 1 if relatively large contours is found
         
         #box all of the relatively large contours found when displaying the frames
         (x, y, w, h) = cv2.boundingRect(countour)
